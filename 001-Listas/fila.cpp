d38ca336ee46d674df8ad85891ccc752cd3f8d5f
Lucas <lucasrodriguezy@gmail.com>
2023-09-13 17:09:55 -0300

    solving exercises

---
+++ 001-Listas/fila.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+
+using namespace :: std;
+int main(){
+	vector<int> fila;
+	int n,i,id;
+	cin >> n;
+	for(i=0;i<n;i++){
+		cin >> id;
+		fila.push_back(id);
+	}
+	int n2;
+	cin >> n2;
+	for(i=0;i<n2;i++){
+		cin >> id;
+		fila.pop_back();
+	}
+	for(int i : fila){
+		cout << i << " ";
+	}
+
+}
